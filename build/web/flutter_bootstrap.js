f80bb51caeb50bd228d8427b8b126cd632a066b7
teja569 <tejasaii1729@gmail.com>
2026-07-18 14:22:09 +0530

    Update service worker version and modify API base URL for production environment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "751855384" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2409224400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
